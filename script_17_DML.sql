413ebb9d8f96b0b845c2410196a0578608789bd6
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2024-07-02 19:46:29 -0300

    ORDER BY - Busca ordenada

--- script_17_DML.sql
+++ script_17_DML.sql
@@ -1,8 +1,19 @@
 /*
-    GROUP BY
+    GROUP BY, HAVING, ORDER BY
     --------------------------------
     A cláusula GROUP BY agrupa linhas e colunas 
     baseado em semelhanças entre elas.
+
+    A cláusula HAVING é usada para especificar condições
+    de filtragem em grupos de registros ou agregações.
+    É frequentemente usada em conjunto com a cláusula GROUP BY
+    para filtrar as colunas agrupadas.
+
+    A palavra-chave ORDER BY é usada para ordenar 
+    o conjunto-resultado de registros em um consulta
+    SQL, tanto de forma ascendente quanto descendente.
+      ASC – Ordem ascendente
+      DESC – Ordem descendente (inversa)
 */
 
 /*SELECT com GROUP BY*/
@@ -50,3 +61,23 @@ inner join
 on c.i_tipo_cliente = t.id_tipo_tipocliente
 group by  i_tipo_cliente
 having t.s_desc_tipocliente != 'PJ';
+
+/*SELECT com ORDER BY*/
+SELECT <campo> FROM <nome_da_tabela> ORDER BY <campo_B>;
+SELECT <campo> FROM <nome_da_tabela> ORDER BY <campo_B> <ORDEM>;
+SELECT <campo> FROM <nome_da_tabela> ORDER BY <campo_B>, <campo_C>;
+SELECT <campo> FROM <nome_da_tabela> ORDER BY <campo_B>, <campo_C> <ORDEM>
+SELECT <campo> FROM <nome_da_tabela> ORDER BY <campo_B> <ORDEM>, <campo_C> <ORDEM>
+
+select * from cliente order by s_nome_cliente;
+select * from cliente order by s_nome_cliente asc;
+select * from cliente order by s_nome_cliente desc;
+select * from cliente order by i_tipo_cliente, s_nome_cliente;
+select * from cliente order by i_tipo_cliente, s_nome_cliente desc;
+select * from cliente order by i_tipo_cliente desc, s_nome_cliente asc;
+
+/*SELECT com ORDER BY e expressões*/
+SELECT <campo> FROM <nome_da_tabela> ORDER BY <expressão>(campo_desejado);
+
+select * from cliente order by right(s_cpf_cliente, 3); /*busca por tres últios digitos do cpf*/
+select * from cliente order by left(s_cpf_cliente, 3); /*busca por tres primeirs digitos do cpf*/
