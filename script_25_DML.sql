588c748ae4a831c076d60c20f09e34f1a1cd9e4c
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2024-07-19 22:25:02 -0300

    Stored Procedure com parametros de saída

--- script_25_DML.sql
+++ script_25_DML.sql
@@ -53,3 +53,50 @@ DELIMITER ;
 
 /*CHAMADA DO PROCEDIMENTO CRIADO ACIMA*/
 CALL <nome_procedimento>(valor_parametro);
+
+/*PASSANDO PARAMENTRO DE SAÍDA*/
+DELIMITER @@
+CREATE PROCEDURE <nome_procedimento>(IN <parametro_in> tipo(tamanho), out <parametro_out> tipo) 
+BEGIN
+    DECLARE <nome_da_variavel> <tipo_de_dado>(tamanho);
+    SET <nome_da_variavel> = VALOR;
+    SET <parametro_out> = VALOR;
+    SELECT <nome_da_variavel>, <parametro>;
+END @@
+DELIMITER ;
+
+CALL <nome_procedimento>(valor_in, @retorno);
+SELECT @retorno;
+
+-- Exemplo --
+DELIMITER @@
+	CREATE PROCEDURE idade(IN idCliente INT, OUT idadeCliente int)
+BEGIN
+	DECLARE data_nasc datetime;
+    SET data_nasc = (SELECT d_nasc_cliente FROM cliente WHERE id_cliente = idCliente);
+    SET idadeCliente = year(now()) - year(data_nasc);
+    
+END @@
+DELIMITER ;
+
+call idade(3,@idadeCliente);
+SELECT @idadeCliente;
+
+/*Exemplo com condicional IF*/
+DELIMITER @@
+	CREATE PROCEDURE idade(IN idCliente INT, OUT idadeCliente int, OUT res varchar(5))
+BEGIN
+	DECLARE data_nasc datetime;
+    SET data_nasc = (SELECT d_nasc_cliente FROM cliente WHERE id_cliente = idCliente);
+    SET idadeCliente = year(now()) - year(data_nasc);
+    IF(idadeCliente >= 18)THEN
+		SET res = "SIM";
+     ELSE
+		SET res = "NÃO";
+    END IF;
+    
+END @@
+DELIMITER ;
+
+call idade(4,@idadeCliente, @maiorIdade);
+select @idadeCliente as 'Idade',@maiorIdade as "Maior Idade";
